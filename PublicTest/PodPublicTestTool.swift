fd00c76e9f3ae052360f6c0a97c35266cf68810f
DylanDeng <dylandeng@vesync.com.cn>
2021-05-28 14:02:04 +0800

    初始化

---
+++ PublicTest/PodPublicTestTool.swift
@@ -0,0 +1,16 @@
+//
+//  PodPubTestTool.swift
+//  VeSyncPlatformSDK
+//
+//  Created by Dylan on 2021/5/28.
+//
+
+import Foundation
+
+public class PodPublicTestTool: NSObject {
+    
+    public static func hello() {
+        print("Hello dylan")
+    }
+    
+}
